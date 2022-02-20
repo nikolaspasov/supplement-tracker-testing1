eeee3419d0b22c1629d801bf0d242a3f78e11d59
nikolaspasov <belinelli100@gmail.com>
2022-02-20 23:36:35 +0200

    Add more basic functionality

--- app.js
+++ app.js
@@ -1,11 +1,76 @@
-let inputElement = document.querySelector(".supplement-value-input-box");
+// HTML ELEMENTS
+
+let quantityInputElement = document.querySelector(".supplement-value-input-box");
 let addButtonElement = document.querySelector(".add-button");
+let addedSupplementsList = document.getElementById("supplements-added-list");
+let supplementButtonElements = document.querySelectorAll('.supplement-button');
+let lastClickedSupplement;
+let supplementArray = [];
+
+// EVENT LISTENERS
+addButtonElement.addEventListener('click', addNewSupplementIntake);
 
 
-addButtonElement.addEventListener('click', printData);
 
-function printData(event){
-event.preventDefault();
-console.log(inputElement.value);
-inputElement.value = null;
+// EVENT LISTENER FOR EVERY SUPPLEMENT BUTTON
+supplementButtonElements.forEach(supplementButton => {
+    supplementButton.addEventListener
+        ('click', function () {
+            lastClickedSupplement = supplementButton.textContent;
+        });
+});
+
+// FUNCTIONS
+function addNewSupplementIntake(event) {
+    event.preventDefault();
+
+    if (lastClickedSupplement == null || quantityInputElement.value == "") {
+        return;
+    }
+    else {
+
+        //If Supplement is already added, increase quantity
+        let alreadyAddedSupplement = document.getElementById(lastClickedSupplement)
+
+        if (alreadyAddedSupplement == null) {
+            let supplementNameElement = document.createElement('li')
+            let supplementQuantityElement = document.createElement('span');
+            let newSuppObject = {
+                name: lastClickedSupplement,
+                quantity: quantityInputElement.value
+            };
+
+            supplementNameElement.textContent = `${lastClickedSupplement} - `
+            supplementQuantityElement.textContent = quantityInputElement.value;
+
+            supplementQuantityElement.setAttribute("id", lastClickedSupplement);
+            supplementNameElement.appendChild(supplementQuantityElement);
+            addedSupplementsList.appendChild(supplementNameElement);
+
+            supplementArray.push(newSuppObject);
+            
+            lastClickedSupplement = null;
+            quantityInputElement.value = null;
+
+        }
+        else {
+            let previousQuantity = parseInt(alreadyAddedSupplement.textContent, 10);
+            let newQuantity = previousQuantity + parseInt(quantityInputElement.value, 10);
+            if (isNaN(newQuantity)) {
+
+                return;
+            }
+            else {
+                alreadyAddedSupplement.textContent = newQuantity;
+            }
+            lastClickedSupplement = null;
+            quantityInputElement.value = null;
+
+        }
+    }
 }
+
+// function getSupplementName(event){
+//   let buttonClicked = event.target;
+//   console.log(buttonClicked);
+// }
